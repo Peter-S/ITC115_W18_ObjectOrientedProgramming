2c9972a0237fc4d3a445ae6082f8048479970fac
Peter-S <32533746+Peter-S@users.noreply.github.com>
2018-02-18 20:41:45 -0800

    update test block

--- VideoNotes/src/VN_TEST_BLOCK.java
+++ VideoNotes/src/VN_TEST_BLOCK.java
@@ -1,16 +1,24 @@
 
+import java.util.*;
 public class VN_TEST_BLOCK {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int sum = 0;
-		
-		for (int i = 1; i <= 100; i++) {
-			sum = sum + i;
-		}
-		System.out.println("the sum is " + sum);
-
+		double[] list1 = {16.1, 12.3, 22.2, 14.4};
+		test (list1);
+		double[] list2 = {1.5, 4.3, 7.0, 19.5, 25.1, 46.2};
+		test (list2);
 	}
-
+	
+	public static void test(double[] list) {
+		System.out.println("Is Sorted "   + isSorted(list));
+	}
+	
+	public static boolean isSorted(double[] list) {
+		for (int i = 0; i < list.length -1; i++) {
+			if (list[i] > list[i + 1])
+				return false;
+				
+		}return true;
+	}
 }
